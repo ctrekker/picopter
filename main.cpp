711c1c32c956147f5fd115f7d7899a2b11a17da0
Connor Burns <ctrekker4@gmail.com>
2021-10-23 20:49:16 +0100

    Add Eigen as library to main and src

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <Eigen/Dense>
 
 #include "src/Quaternion.h"
+
+using Eigen::MatrixXd;
 
 int main()
 {
@@ -10,4 +13,11 @@ int main()
     Quaternion q2(0., 1., 0., 0.);
 
     std::cout << (q1 + q2) << std::endl;
+
+MatrixXd m(2,2);
+  m(0,0) = 3;
+    m(1,0) = 2.5;
+      m(0,1) = -1;
+        m(1,1) = m(1,0) + m(0,1);
+	  std::cout << m << std::endl;
 }
