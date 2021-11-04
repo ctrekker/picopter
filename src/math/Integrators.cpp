19bad5dbad6dcc1ecbf2de2405e00913af7261d3
Connor Burns <ctrekker4@gmail.com>
2021-11-03 18:15:58 -0600

    Rolling simpson integrator take 2

--- src/math/Integrators.cpp
+++ src/math/Integrators.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include "Integrators.h"
+#include <math.h>
 
 // TODO: Remove main method during pi integration
 int main() {
     float dt = 0.1;
 
+    SimpsonIntegrator<float> simpInt = SimpsonIntegrator<float>(dt);
     TrapezoidalIntegrator<float> trapInt = TrapezoidalIntegrator<float>(dt);
-    Integrator<float>* integrator = dynamic_cast<Integrator<float>*>(&trapInt);
 
-    float maxTime = 1;
+    float maxTime = M_PI;
 
     for(int _t=0; _t<(maxTime / dt + 1); _t++) {
         float t = (float)_t * dt;
-        integrator->step(t * t);
+        std::cout << "t " << t << std::endl;
+        float val = sin(t);
+        trapInt.step(val);
+        simpInt.step(val);
+        std::cout << "TRAP " << trapInt.result() << "\t" << "SIMP " << simpInt.result() << std::endl;
     }
-
-    std::cout << integrator->result() << std::endl;
 
     return 0;
 }
@@ -41,4 +44,47 @@ void TrapezoidalIntegrator<T>::step(T nextValue) {
 template <typename T>
 T TrapezoidalIntegrator<T>::result() {
     return aggregate + Integrator<T>::dt * (lastValue + firstValue) / 2;
+}
+
+
+template <typename T>
+void SimpsonIntegrator<T>::step(T nextValue) {
+    if(!hasFirstValue) {
+        firstValue = nextValue;
+        hasFirstValue = true;
+    }
+    else {
+        if(sumState == 2) {
+            aggregate += Integrator<T>::dt * (3 * values[0] + 3 * values[1]);
+            if(currentStep != 3) {
+                aggregate += Integrator<T>::dt * values[2];
+            }
+
+            values[2] = nextValue;
+            aggregate += Integrator<T>::dt * values[2];
+
+            sumState = 0;
+        }
+        else {
+            values[sumState] = nextValue;
+            sumState++;
+        }
+    }
+    
+    currentStep++;
+}
+
+template <typename T>
+T SimpsonIntegrator<T>::result() {
+    int rem = (currentStep - 1) % 3;
+    T adjustment = 0;
+    if(rem == 1) {
+        // approximate the last part with trapezoidal
+        adjustment = Integrator<T>::dt * (values[2] + values[0]) / 2;
+    }
+    else if(rem == 2) {
+        // approximate the last part with 1/3 simpson's rule
+        adjustment = Integrator<T>::dt * (values[2] + 4 * values[0] + values[1]) / 3;
+    }
+    return (3./8) * aggregate + adjustment;
 }
--- src/math/Integrators.h
+++ src/math/Integrators.h
@@ -24,3 +24,20 @@ class TrapezoidalIntegrator: public Integrator<T> {
         void step(T nextValue);
         T result();
 };
+
+template <typename T>
+class SimpsonIntegrator: public Integrator<T> {
+    private:
+        uint64_t currentStep = 0;
+        bool hasFirstValue = false;
+        int sumState = 0;
+        T firstValue = 0;
+        T aggregate = 0;
+
+        T values[3]{0, 0, 0};
+    public:
+        SimpsonIntegrator(T dt) : Integrator<T>(dt) {};
+
+        void step(T nextValue);
+        T result();
+};
