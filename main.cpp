d99cfeab03dcb843e80b9d328260ca6e43b987e7
Connor Burns <ctrekker4@gmail.com>
2021-11-19 23:15:29 +0000

    Ble server compiled into picopter

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include "src/drone.h"
+// #include "src/drone.h"
+#include "src/ble/bleserver.h"
 
-int main() {
-    init();
-    runEventLoop();
+int main(int argc, char **ppArgv) {
+    // init();
+    // runEventLoop();
+
+    blemain(argc, ppArgv);
 
     return 0;
 }
---
+++ src/ble/bleserver.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int blemain(int argc, char **ppArgv);
